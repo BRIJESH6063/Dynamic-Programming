c28ab9abfe2bd7e49b3874b83217a335157fef4d
BRIJESH KUMAR YADAV <103829299+BRIJESH6063@users.noreply.github.com>
2022-07-25 11:56:35 +0530

    Top down approach of Dynamic Programming
    Top down approach of Dynamic Programming to solve Fibonacci series problem in linear time complexity..

---
+++ TopDownMemo.cpp
@@ -0,0 +1,31 @@
+// Dynamic Programming Introduction.. 
+
+#include <iostream>
+using namespace std;
+const int N = 1e5+10 ;
+
+int dp[N] ;
+int fibo(int n)
+{
+    if(n == 1 or n == 0) return n ;
+    if(dp[n] != -1) return dp[n] ;
+    return dp[n] = fibo(n-1)+fibo(n-2) ;
+    
+}
+
+int main() {
+    for(int i=0; i<N; i++) dp[i] = -1 ;
+
+	cout << n << endl ;
+	cout << fibo(n) << endl ;
+	
+	
+	return 0;
+}
+
+
+/*
+memset dont work for random positive number to be filled.
+generally suggested to fill by -1.
+Time Complexity -- > T(n) = O(n) ..
+*/

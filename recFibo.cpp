c95ee97b7f9554a611f64853d501090ad70e87b7
BRIJESH KUMAR YADAV <103829299+BRIJESH6063@users.noreply.github.com>
2022-07-25 11:47:01 +0530

    Dynamic Programming Introduction..
    Why we need Dynamic Programming...

---
+++ recFibo.cpp
@@ -0,0 +1,27 @@
+// Dynamic Programming Introduction.. 
+// Why we need Dynamic Programming...
+
+#include <iostream>
+using namespace std;
+const int N = 1e5+10 ;
+
+int fibo(int n)
+{
+    if(n == 1 or n == 0) return n ;
+    return fibo(n-1)+fibo(n-2) ;
+    
+}
+
+int main() {
+	int n ; cin >> n ;
+	cout << n << endl ;
+	cout << fibo(n) << endl ;
+	
+	
+	return 0;
+}
+
+
+/*
+Recursion method Time Complexity -- T(n) = 2^n (Exponential).. 
+*/
